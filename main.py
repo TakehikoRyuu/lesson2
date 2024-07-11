40fb7b3a4932a62a8b2ef638e9202aeb58c89272
Андрей Шеремет <seheremet.and@yandex.ru>
2024-07-11 13:08:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ number_of_tasks = 12
 number_of_hours = 1.5
 average_time = (number_of_hours / number_of_tasks)
 
-print('Курс:', first_name, ',', 'всего задач:' ,number_of_tasks, ',', 'затрачено часов:' ,number_of_hours, ',', 'среднее время выполнения', average_time, 'часа.')
+print('Курс:', first_name, ',', 'всего задач:', number_of_tasks, ',', 'затрачено часов:', number_of_hours, ',', 'среднее время выполнения', average_time, 'часа.')
 
 
 
