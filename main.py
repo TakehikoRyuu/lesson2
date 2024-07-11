243d08cd7886a398da0b5631c5b91835bb6540c6
Андрей Шеремет <seheremet.and@yandex.ru>
2024-07-11 13:06:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
-
 # Практическое задание по теме "Переменные."
-# Вариант 1
-first_name = 'Курс:'
-last_name = 'Python,'
-number_of_tasks = '12,'
-number_of_hours = '1.5,'
-average_time = 0.125
-print(first_name, last_name,'всего задач:',number_of_tasks, 'затрачено часов:',number_of_hours, 'среднее время выполнения',average_time, 'часа.')
+first_name = 'Python'
+number_of_tasks = 12
+number_of_hours = 1.5
+average_time = (number_of_hours / number_of_tasks)
 
-#вариант 2
-a = 'Курс:'
-b = 'Python,'
-c = 'всего задач: 12,'
-d = 'затрачено часов: 1.5,'
-e = 'среднее время выполнения 0.125 часа.'
-print (a, b, c, d, e)
+print('Курс:', first_name, ',', 'всего задач:' ,number_of_tasks, ',', 'затрачено часов:' ,number_of_hours, ',', 'среднее время выполнения', average_time, 'часа.')
+
 
 
